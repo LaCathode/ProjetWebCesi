f7520c544c0dc8d225eb1504d278aa753b7769cb
dagdelensuleyman <39514867+dagdelensuleyman@users.noreply.github.com>
2018-05-25 16:54:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
 $adresse_ip = $_SERVER['REMOTE_ADDR'];
 
 function get_ip() {
-	// IP si internet partagé
-	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-		return $_SERVER['HTTP_CLIENT_IP'];
-	}
-	// IP derrière un proxy
-	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-		return $_SERVER['HTTP_X_FORWARDED_FOR'];
-	}
-	// Sinon : IP normale
-	else {
-		return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
-	}
+    // IP si internet partagé
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    }
+    // IP derrière un proxy
+    elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    // Sinon : IP normale
+    else {
+        return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
+    }
 }
 
 
@@ -29,7 +29,7 @@ echo 'Adresse IP du visiteur : '.get_ip();
 
 <?php
 
-	session_start();
+    session_start();
 
 if(!isset($_SESSION['tab'])){
 
@@ -68,13 +68,13 @@ echo '</pre>';
 
 <?php 
 
-	$user = "root";
-	$pass = "";
+    $user = "root";
+    $pass = "root";
 
 $c = 0;
 if($_SESSION['q']>4){
 
-	$_SESSION['q'] = 0;
+    $_SESSION['q'] = 0;
 }
 $q = $_SESSION['q'];
 
@@ -88,16 +88,44 @@ try {
         echo "<br>";
 
 
-        	foreach($dbh->query('SELECT rep  from questions,reponses where questions.id_q = reponses.id_q and questions.id_q="'.$_SESSION['tab'][$q].'"') as $row) {
+            foreach($dbh->query('SELECT rep  from questions,reponses where questions.id_q = reponses.id_q and questions.id_q="'.$_SESSION['tab'][$q].'"') as $row) {
+
+
+                foreach($dbh->query('SELECT bool_pl FROM questions WHERE id_q ="'.$_SESSION['tab'][$q].'"')as $row2) ;
+
+
+                    
+foreach($dbh->query('SELECT bool_rep FROM reponses,questions WHERE reponses.id_q = questions.id_q and reponses.id_q ="'.$_SESSION['tab'][$q].'"')as $row3) {
+    print_r($row3[$c]);
+
+
+                    print_r($row2[$c]);
+                    echo "&nbsp";
+                  
+          
+    
+}
+
+
+
+
         
-
-
         echo "<br>";
-        print_r($row[$c]);
-        echo "<br>";
+        echo "<form method='post'>";
+        if($row2[$c]==1){
 
+        echo"<input type='checkbox' name='' text=''> ";  print_r($row[$c]); 
+        echo "<br>";
+        }else{
+
+        echo"<input type='radio' name='reponse' text=''> ";  print_r($row[$c]); 
+        echo "<br>";
         }
-    }
+        }
+    
+
+    
+}
 
     $dbh = null;
 
@@ -108,7 +136,7 @@ try {
 
 ?>
 
-<form method='post'>
+
 <button name='valider'>Valider</button>
 </form>
 
