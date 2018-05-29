d266aaabaa46837f96607fd65ae320021b1e7425
LaCathode <39514879+LaCathode@users.noreply.github.com>
2018-05-29 10:41:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
 <?php
 /*
 $adresse_ip = $_SERVER['REMOTE_ADDR'];
@@ -29,22 +30,26 @@ echo 'Adresse IP du visiteur : '.get_ip();
 
 <?php
 
-    session_start();
+session_start();
+
+//Si le tableau dans la session est vide on le remplie avec 5 valeur aléatoire compris entre 1 et 20
 
 if(!isset($_SESSION['tab'])){
 
 $_SESSION['q'] = 0;
+
 $max=20;
 
 $_SESSION['tab'] = array();
 
 $i = 0;
+
 while ($i<6)
 
 {
     $image = rand(1, $max);
      
-    if (!in_array($image, $tableau)) {
+    if (!in_array($image, $_SESSION['tab'])) {
         $_SESSION['tab'][] = $image;       
             $i++;
     }
@@ -55,100 +60,125 @@ while ($i<6)
 echo '<pre>';
 print_r($_SESSION['tab']);
 echo '</pre>';
+ 
    
 }else{
+
 
 echo '<pre>';
 print_r($_SESSION['tab']);
 echo '</pre>';
 
- };
-
-?>
-
-<?php 
-
-    $user = "root";
-    $pass = "root";
-
-$c = 0;
-if($_SESSION['q']>4){
-
-    $_SESSION['q'] = 0;
-}
-$q = $_SESSION['q'];
-
-
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=projetwebcesi', $user, $pass);
-    foreach($dbh->query('SELECT question  from questions where  questions.id_q="'.$_SESSION['tab'][$q].'"') as $row) {
-        
-        echo "<br>";
-        print_r($row[$c]);
-        echo "<br>";
-
-
-            foreach($dbh->query('SELECT rep  from questions,reponses where questions.id_q = reponses.id_q and questions.id_q="'.$_SESSION['tab'][$q].'"') as $row) {
-
-
-                foreach($dbh->query('SELECT bool_pl FROM questions WHERE id_q ="'.$_SESSION['tab'][$q].'"')as $row2) ;
-
-
-                    
-foreach($dbh->query('SELECT bool_rep FROM reponses,questions WHERE reponses.id_q = questions.id_q and reponses.id_q ="'.$_SESSION['tab'][$q].'"')as $row3) {
-    print_r($row3[$c]);
-
-
-                    print_r($row2[$c]);
-                    echo "&nbsp";
-                  
-          
-    
-}
-
-
-
-
-        
-        echo "<br>";
-        echo "<form method='post'>";
-        if($row2[$c]==1){
-
-        echo"<input type='checkbox' name='' text=''> ";  print_r($row[$c]); 
-        echo "<br>";
-        }else{
-
-        echo"<input type='radio' name='reponse' text=''> ";  print_r($row[$c]); 
-        echo "<br>";
-        }
-        }
-    
-
-    
-}
-
-    $dbh = null;
-
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
 };
 
 ?>
 
+<?php 
+  
+$c = 0;
+
+//Si le compteur stocker dans la session est supérieur à 4 on réintialise le compteur à 0
+//Car y a seulement 5 questions à afficher
+
+if($_SESSION['q']>4){
+
+    $_SESSION['q'] = 0;
+}
+
+$q = $_SESSION['q'];
+
+try {
+
+	$user = "root";
+    $pass = "";
+
+
+	//Récupère les questions 
+    $dbh = new PDO('mysql:host=localhost;dbname=projetwebcesi', $user, $pass);
+    foreach($dbh->query('SELECT question  from questions where  questions.id_q="'.$_SESSION['tab'][$q].'"') as $row) {
+        
+
+        //Affiche la question 
+        echo "<br>";
+        print_r($row[$c]);
+        echo "<br>";
+
+        	//Récupère les réponses
+            foreach($dbh->query('SELECT rep  from questions,reponses where questions.id_q = reponses.id_q and questions.id_q="'.$_SESSION['tab'][$q].'"') as $row2) {
+
+
+            	//Récupére le booléen qui permet de savoir s'il existe plusieurs réponses juste
+                foreach($dbh->query('SELECT bool_pl FROM questions WHERE id_q ="'.$_SESSION['tab'][$q].'"')as $row3) ;
+
+
+                    		//Récupére le boolén bool_rep
+							foreach($dbh->query('SELECT bool_rep FROM reponses,questions WHERE reponses.id_q = questions.id_q and rep= "'.$row2[$c].'" and reponses.id_q ="'.$_SESSION['tab'][$q].'"')as $row4);
+
+ 
+					//Affiche les différents booléen pour tester
+    				echo "bool_pl  ";
+                    print_r($row3[$c]);
+                    echo "&nbsp  ";
+                    echo "  bool_rep  ";
+                    print_r($row4[$c]); 
+                    
+          
+        //Si le booléen bool_pl est à 1 celà veux dire qu'il y a plusieurs réponses possible donc on afficher des checkbox
+        echo "<br>";
+        echo "<form method='post'>";
+
+        if($row3[$c]==1){
+
+        echo"<input type='checkbox' name='' text=''> ";  print_r($row2[$c]); 
+        echo "<br>";
+
+         }
+
+         //Sinon le booléen est à 0 donc il y a qu'une seule réponse possible donc on affiche des radios button
+                  else{
+
+        echo"<input type='radio' name='reponse' text=''> ";  print_r($row2[$c]); 
+        echo "<br>";
+
+        }
+        }
+       
+}
+
+
+
+
+    $dbh = null;
+
+} catch (PDOException $e) {
+
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+
+};
+
+?>
 
 <button name='valider'>Valider</button>
+
 </form>
 
+<?php
+
+
+?>
 
 <?php
+
+	//Si l'utilisateur click sur le boutton valider on incrémente le compteur pour passer à la question suivante
     if(isset($_POST['valider']) )
     {
         
-        echo $_SESSION['q'];echo $_SESSION['tab'][$_SESSION['q']];
+        echo $_SESSION['q'];echo "   ";echo $_SESSION['tab'][$_SESSION['q']];
         echo "<br>";
         $_SESSION['q'] = $_SESSION['q']+1;
-        echo $_SESSION['q'];echo $_SESSION['tab'][$_SESSION['q']];
+       // echo $_SESSION['q'];echo "   ";echo $_SESSION['tab'][$_SESSION['q']];
 
     }
-?>
+
+ ?>
